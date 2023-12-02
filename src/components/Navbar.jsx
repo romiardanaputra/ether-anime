78eb2793ec154b58550dd7b84111b2c740e45cf9
Kadek Romi Ardana Putra <romiardana21@gmail.com>
2023-12-02 08:04:21 +0800

    adding navbar and setup route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+   return (
+      <>
+         
+      </>
+   );
+};
 
-export default Navbar
+export default Navbar;
