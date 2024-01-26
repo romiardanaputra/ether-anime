f0fd8e9f1ea545dba3c9052692f22008aa8f0292
FredySetiawan <fredysetiawan171@gmail.com>
2024-01-26 19:18:37 +0800

    update v1
    pembaruan style

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
    return (
       <>
-         <div className="text-blue-700 font-normal text-[4rem] font-inter">Hello Ether Anime</div>
+         {/* <div className="text-blue-700 font-normal text-[4rem] font-inter">Hello Ether Anime</div> */}
       </>
    );
 };
