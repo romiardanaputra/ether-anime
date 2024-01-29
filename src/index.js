28d4b5d1f3081b9e09114000deb4fc32510e206c
FredySetiawan <fredysetiawan171@gmail.com>
2024-01-29 19:37:54 +0800

    Update Ui

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
-import {Home} from './pages'
+import {Home, Login} from './pages'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,6 +10,7 @@ root.render(
    <Router>
       <Routes>
          <Route index path="/" element={<Home />}/>
+         <Route path="/login" element={<Login/>}/>
       </Routes>
    </Router>
 );
