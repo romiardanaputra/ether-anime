67f8e5950561e4c17f7d6355b5507b012433fb49
FredySetiawan <fredysetiawan171@gmail.com>
2024-02-05 07:49:39 +0800

    update password

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -31,7 +31,7 @@ const login = () => {
                                     d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
                                     clip-rule="evenodd" />
                             </svg>
-                            <input class="pl-2 outline-none border-none login" type="text" name="" id="" placeholder="Password" />
+                            <input class="pl-2 outline-none border-none login" type="password" name="" id="" placeholder="Password" />
                         </div>
                         <button type="submit" class="px-20 text-white bg-cyan-600 hover:bg-blue-800 focus:ring-4 w-52 focus:ring-blue-300 font-medium rounded-2xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">LOGIN</button>
                         <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer">Forgot Password ?</span>
